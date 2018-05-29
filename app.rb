7b39b486a0cc74289b0ec53e0ec5463c6fde679d
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-29 15:00:18 +0100

    added dog functionality

--- app.rb
+++ app.rb
@@ -11,6 +11,9 @@ end
 
 get '/evenmoresecret' do
   "super secret stuff"
+  "<div style='border: 100px dotted turquoise'>
+    <img src='https://pbs.twimg.com/profile_images/948761950363664385/Fpr2Oz35_400x400.jpg'>
+    </div>"
 end
 
 get '/morestuff' do
@@ -18,7 +21,7 @@ get '/morestuff' do
 end
 
 get '/cat' do
-  "<div style='border: 10px dotted turquoise'>
+  "<div style='border: 100px dotted turquoise'>
     <img src='http://bit.ly/1eze8aE'>
     </div>"
 end
